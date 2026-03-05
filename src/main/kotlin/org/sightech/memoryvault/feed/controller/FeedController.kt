96b14936a1f7df864a396dbcea31e0aa5f28598b
Sigh <sighonara@gmail.com>
2026-03-05 14:25:35 -0800

    feat: add FeedController stub with list endpoint

---
+++ src/main/kotlin/org/sightech/memoryvault/feed/controller/FeedController.kt
@@ -0,0 +1,14 @@
+package org.sightech.memoryvault.feed.controller
+
+import org.sightech.memoryvault.feed.service.FeedService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/feeds")
+class FeedController(private val service: FeedService) {
+
+    @GetMapping
+    fun listFeeds() = service.listFeeds().map { (feed, unread) ->
+        mapOf("id" to feed.id, "url" to feed.url, "title" to feed.title, "unreadCount" to unread)
+    }
+}
