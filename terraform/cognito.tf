f8d7a6fc5e82909651dca3997200f2a70eb295cf
Sigh <sighonara@gmail.com>
2026-04-15 13:46:04 -0700

    feat: add Terraform Cognito User Pool and SPA client

---
+++ terraform/cognito.tf
@@ -0,0 +1,55 @@
+resource "aws_cognito_user_pool" "main" {
+  name = "${var.project_name}-users"
+
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+  }
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_uppercase = true
+    require_numbers   = true
+    require_symbols   = true
+  }
+
+  schema {
+    name                = "role"
+    attribute_data_type = "String"
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 20
+    }
+  }
+
+  tags = {
+    Name = "${var.project_name}-cognito"
+  }
+}
+
+resource "aws_cognito_user_pool_client" "spa" {
+  name         = "${var.project_name}-spa"
+  user_pool_id = aws_cognito_user_pool.main.id
+
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
+
+  access_token_validity  = 1
+  id_token_validity      = 1
+  refresh_token_validity = 30
+}
